1ba982abd069832c876ab20cd517c0f395f5f011
Frankz115935 <161490440+Frankz115935@users.noreply.github.com>
2024-07-02 00:22:03 -0600

    Programa 19 – Conjuntos Práctica

---
+++ Ejercicio_19.py
@@ -0,0 +1,7 @@
+conjunto1 = {"manzana", "guayaba", "pera"}
+conjunto2 = {"naranja", "uva", "mandarina"}
+conjunto3 = conjunto1.union(conjunto2)
+print("Union de los conjuntos: ", conjunto3)
+conjunto3.add("banano")
+conjunto_final = conjunto3
+print("Conjunto final: ",conjunto_final)
