eba38599ea99ba80599d40cd7d197f171f83c8de
Frankz115935 <161490440+Frankz115935@users.noreply.github.com>
2024-07-02 00:23:43 -0600

    Programa 21 – Tupla desempaquetada

---
+++ Ejercicio_21.py
@@ -0,0 +1,13 @@
+informacion_personal = ("Francisco Perez Figueroa", 20, 151537126, "14/04/2004", "PEFF0202HDGRGRA4")
+
+Nombre = informacion_personal[0]
+Edad = informacion_personal[1]
+Matricula = informacion_personal[2]
+Fecha_de_nac = informacion_personal[3]
+CURP = informacion_personal[4]
+
+print("Nombre: ", Nombre)
+print("Edad: ", Edad)
+print("Matricula: ", Matricula)
+print("Fecha de Nacimiento: ", Fecha_de_nac)
+print("CURP", CURP)
